f05d76925224bbf0dd8b831a24a42233607bdc86
deahlt <devdeahaliti@gmail.com>
2021-02-14 19:11:01 +0100

    added dashboard.php and css

---
+++ dashboard.php
@@ -0,0 +1,62 @@
+<?php
+session_start();
+ob_start();
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/dashboard.css">
+    <title>PETLOVE</title>
+</head>
+
+<body>
+    
+
+<header>
+	</header>
+	<div class="container">
+		
+		<div class="sidebar">
+			<nav>
+				<a href="#" class="logo">PETLOVE</a>
+				<ul>
+					<li class="active"><a href="#">Dashboard</a></li>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="news.php">News</a></li>
+					<li><a href="admin.php">Add Pet</a></li>
+				</ul>
+			</nav>
+		</div>
+
+		<div class="main-content">
+			<h1>Dashboard</h1>
+			<p>Pjesa e admin-it</p>
+			<div class="panel-wrapper">
+				<div class="panel-head">
+					News
+				</div>
+				<div class="panel-body">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam fugiat culpa quia possimus molestiae id sapiente ad eveniet, aliquid, eum sint fuga eius, ratione suscipit ut minus voluptates dicta nesciunt.
+				</div>
+			</div>
+			<div class="panel-wrapper">
+				<div class="panel-head">
+					News
+				</div>
+				<div class="panel-body">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam fugiat culpa quia possimus molestiae id sapiente ad eveniet, aliquid, eum sint fuga eius, ratione suscipit ut minus voluptates dicta nesciunt.
+				</div>
+			</div>
+			<div class="panel-wrapper">
+				<div class="panel-head">
+					News
+				</div>
+				<div class="panel-body">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam fugiat culpa quia possimus molestiae id sapiente ad eveniet, aliquid, eum sint fuga eius, ratione suscipit ut minus voluptates dicta nesciunt. Totam fugiat culpa quia possimus molestiae id sapiente ad eveniet, aliquid, eum sint fuga eius, ratione suscipit ut minus voluptates dicta nesciunt.
+				</div>
+			</div>
+		</div>
+	</div>
+    </body>
+    </html>
