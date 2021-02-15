5aab6a299788ecad034671d4071a86fe2d2b8b7a
devlorik <devlorikhalili@gmail.com>
2021-02-15 23:12:36 +0100

    Added Slider v2

--- pets.php
+++ pets.php
@@ -8,25 +8,33 @@ include 'header.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/pets.css">
-    <title>PETLOVE</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
-<main>
+<body>
     <!--SLIDER-->
-    <section class="auto-slider">
+    <section class="slider">
         <h1>Disa nga miqtë tanë!</h1>
-        <div id="slider">
-            <figure>
-                <img src="img/dog1.jpg" alt="Image">
-                <img src="img/dog2.jpg" alt="Image">
-                <img src="img/cat1.jpg" alt="Image">
-                <img src="img/dog4.jpg" alt="Image">
-                <img src="img/cat2.jpg" alt="Image">
-            </figure>
+        <div class="carousel">
+            <a href="#" class="slide-left"> <i class="fa fa-arrow-left"></i> </a>
+            <div class="carousel-item-wrapper">
+                <img src="img/dog1.jpg" alt="" class="carousel-item">
+                <img src="img/dog2.jpg" alt="" class="carousel-item">
+                <img src="img/cat1.jpg" alt="" class="carousel-item">
+                <img src="img/dog4.jpg" alt="" class="carousel-item">
+                <img src="img/cat2.jpg" alt="" class="carousel-item">
+            </div>
+            <a href="#" class="slide-right"> <i class="fa fa-arrow-right"></i> </a>
+
         </div>
     </section>
-    <!--SLIDER END-->
-</main>
+
+
+
+    <script src="js/pets.js"></script>
+
+
+</body>
 
 <?php
 include 'footer.php';
